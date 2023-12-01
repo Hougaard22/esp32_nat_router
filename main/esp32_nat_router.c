2120f3e1c1c49343d54b55305dbea78d33635b5a
Jacob Pedersen <jacobh_pedersen@hotmail.com>
2023-12-01 09:49:38 +0100

    Cleanup

--- main/esp32_nat_router.c
+++ main/esp32_nat_router.c
@@ -236,76 +236,89 @@ esp_err_t del_portmap(u8_t proto, u16_t mport) {
     return ESP_OK;
 }
 
-static void initialize_console(void) {
-    // /* Disable buffering on stdin */
+static void initialize_console(void)
+{
+    /* Disable buffering on stdin */
     setvbuf(stdin, NULL, _IONBF, 0);
 
 #if CONFIG_ESP_CONSOLE_UART_DEFAULT || CONFIG_ESP_CONSOLE_UART_CUSTOM
     /* Drain stdout before reconfiguring it */
     fflush(stdout);
     fsync(fileno(stdout));
-     /* Minicom, screen, idf_monitor send CR when ENTER key is pressed */
-     esp_vfs_dev_uart_port_set_rx_line_endings(0, ESP_LINE_ENDINGS_CR);
-     /* Move the caret to the beginning of the next line on '\n' */
-     esp_vfs_dev_uart_port_set_tx_line_endings(0, ESP_LINE_ENDINGS_CRLF);
+    
+    /* Minicom, screen, idf_monitor send CR when ENTER key is pressed */
+    esp_vfs_dev_uart_port_set_rx_line_endings(0, ESP_LINE_ENDINGS_CR);
+    /* Move the caret to the beginning of the next line on '\n' */
+    esp_vfs_dev_uart_port_set_tx_line_endings(0, ESP_LINE_ENDINGS_CRLF);
+
     /* Configure UART. Note that REF_TICK is used so that the baud rate remains
      * correct while APB frequency is changing in light sleep mode.
      */
     const uart_config_t uart_config = {
-    .baud_rate = CONFIG_ESP_CONSOLE_UART_BAUDRATE,
-    .data_bits = UART_DATA_8_BITS,
-    .parity = UART_PARITY_DISABLE,
-    .stop_bits = UART_STOP_BITS_1,
-    #if defined(CONFIG_IDF_TARGET_ESP32) || defined(CONFIG_IDF_TARGET_ESP32S2)
-    .source_clk = UART_SCLK_REF_TICK,
-    #else
-    .source_clk = UART_SCLK_XTAL,
-    #endif
+            .baud_rate = CONFIG_ESP_CONSOLE_UART_BAUDRATE,
+            .data_bits = UART_DATA_8_BITS,
+            .parity = UART_PARITY_DISABLE,
+            .stop_bits = UART_STOP_BITS_1,
+            #if defined(CONFIG_IDF_TARGET_ESP32) || defined(CONFIG_IDF_TARGET_ESP32S2)
+                .source_clk = UART_SCLK_REF_TICK,
+            #else
+                .source_clk = UART_SCLK_XTAL,
+            #endif
     };
     /* Install UART driver for interrupt-driven reads and writes */
     ESP_ERROR_CHECK( uart_driver_install(CONFIG_ESP_CONSOLE_UART_NUM,
-        256, 0, 0, NULL, 0) );
+            256, 0, 0, NULL, 0) );
     ESP_ERROR_CHECK( uart_param_config(CONFIG_ESP_CONSOLE_UART_NUM, &uart_config) );
-     /* Tell VFS to use UART driver */
-     esp_vfs_dev_uart_use_driver(CONFIG_ESP_CONSOLE_UART_NUM);
+
+    /* Tell VFS to use UART driver */
+    esp_vfs_dev_uart_use_driver(CONFIG_ESP_CONSOLE_UART_NUM);
 #endif
 
 #if CONFIG_ESP_CONSOLE_USB_SERIAL_JTAG
+    /* Enable non-blocking mode on stdin and stdout */
     fcntl(fileno(stdout), F_SETFL, O_NONBLOCK);
     fcntl(fileno(stdin), F_SETFL, O_NONBLOCK);
+
     /* Minicom, screen, idf_monitor send CR when ENTER key is pressed */
     esp_vfs_dev_usb_serial_jtag_set_rx_line_endings(ESP_LINE_ENDINGS_CR);
+
     /* Move the caret to the beginning of the next line on '\n' */
     esp_vfs_dev_usb_serial_jtag_set_tx_line_endings(ESP_LINE_ENDINGS_CRLF);
     usb_serial_jtag_driver_config_t usb_serial_jtag_config = {
         .tx_buffer_size = 256,
         .rx_buffer_size = 256,
     };
+
     /* Install USB-SERIAL-JTAG driver for interrupt-driven reads and writes */
     usb_serial_jtag_driver_install(&usb_serial_jtag_config);
+
     /* Tell vfs to use usb-serial-jtag driver */
     esp_vfs_usb_serial_jtag_use_driver();
 #endif
 
     /* Initialize the console */
     esp_console_config_t console_config = {
-    .max_cmdline_args = 8,
-    .max_cmdline_length = 256,
+            .max_cmdline_args = 8,
+            .max_cmdline_length = 256,
 #if CONFIG_LOG_COLORS
-    .hint_color = atoi(LOG_COLOR_CYAN)
+            .hint_color = atoi(LOG_COLOR_CYAN)
 #endif
     };
     ESP_ERROR_CHECK( esp_console_init(&console_config) );
+
     /* Configure linenoise line completion library */
     /* Enable multiline editing. If not set, long commands will scroll within
      * single line.
      */
     linenoiseSetMultiLine(1);
+
     /* Tell linenoise where to get command completions and hints */
     linenoiseSetCompletionCallback(&esp_console_get_completion);
     linenoiseSetHintsCallback((linenoiseHintsCallback*) &esp_console_get_hint);
-     /* Set command history size */
-     linenoiseHistorySetMaxLen(100);
+
+    /* Set command history size */
+    linenoiseHistorySetMaxLen(100);
+
 #if CONFIG_STORE_HISTORY
     /* Load command history from filesystem */
     linenoiseHistoryLoad(HISTORY_PATH);
@@ -604,46 +617,46 @@ void app_main(void)
     register_router();
 
     /* Prompt to be printed before each line.
-    * This can be customized, made dynamic, etc.
-    */
+     * This can be customized, made dynamic, etc.
+     */
     const char* prompt = LOG_COLOR_I "esp32> " LOG_RESET_COLOR;
 
     printf("\n"
-            "ESP32 NAT ROUTER\n"
-            "Type 'help' to get the list of commands.\n"
-            "Use UP/DOWN arrows to navigate through command history.\n"
-            "Press TAB when typing command name to auto-complete.\n");
+           "ESP32 NAT ROUTER\n"
+           "Type 'help' to get the list of commands.\n"
+           "Use UP/DOWN arrows to navigate through command history.\n"
+           "Press TAB when typing command name to auto-complete.\n");
 
     if (strlen(ssid) == 0) {
-        printf("\n"
-                "Unconfigured WiFi\n"
-                "Configure using 'set_sta' and 'set_ap' and restart.\n");       
+         printf("\n"
+               "Unconfigured WiFi\n"
+               "Configure using 'set_sta' and 'set_ap' and restart.\n");       
     }
 
     /* Figure out if the terminal supports escape sequences */
     int probe_status = linenoiseProbe();
     if (probe_status) { /* zero indicates success */
         printf("\n"
-                "Your terminal application does not support escape sequences.\n"
-                "Line editing and history features are disabled.\n"
-                "On Windows, try using Putty instead.\n");
+               "Your terminal application does not support escape sequences.\n"
+               "Line editing and history features are disabled.\n"
+               "On Windows, try using Putty instead.\n");
         linenoiseSetDumbMode(1);
 #if CONFIG_LOG_COLORS
         /* Since the terminal doesn't support escape sequences,
-            * don't use color codes in the prompt.
-            */
+         * don't use color codes in the prompt.
+         */
         prompt = "esp32> ";
 #endif //CONFIG_LOG_COLORS
-        }
-        linenoiseAllowEmpty(0);
-        /* Main loop */
+    }
+
+    /* Main loop */
     while(true) {
         /* Get a line using linenoise.
-            * The line is returned when ENTER is pressed.
-            */
+         * The line is returned when ENTER is pressed.
+         */
         char* line = linenoise(prompt);
         if (line == NULL) { /* Ignore empty lines */
-                continue;
+            continue;
         }
         /* Add the command to the history */
         linenoiseHistoryAdd(line);
@@ -656,13 +669,13 @@ void app_main(void)
         int ret;
         esp_err_t err = esp_console_run(line, &ret);
         if (err == ESP_ERR_NOT_FOUND) {
-                printf("Unrecognized command\n");
+            printf("Unrecognized command\n");
         } else if (err == ESP_ERR_INVALID_ARG) {
-                // command was empty
+            // command was empty
         } else if (err == ESP_OK && ret != ESP_OK) {
-                printf("Command returned non-zero error code: 0x%x (%s)\n", ret, esp_err_to_name(ret));
+            printf("Command returned non-zero error code: 0x%x (%s)\n", ret, esp_err_to_name(ret));
         } else if (err != ESP_OK) {
-                printf("Internal error: %s\n", esp_err_to_name(err));
+            printf("Internal error: %s\n", esp_err_to_name(err));
         }
         /* linenoise allocates line buffer on the heap, so need to free it */
         linenoiseFree(line);
